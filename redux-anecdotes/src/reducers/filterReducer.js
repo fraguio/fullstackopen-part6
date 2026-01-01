ec2719656c76fcf8ea4858c164fe5862a20553ff
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2026-01-02 00:40:59 +0100

    refactor(ex. 6.10): migrate Redux setup to Redux Toolkit and enable DevTools

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,15 +1,16 @@
-export const filterAnecdotes = (filter) => ({
-  type: "SET_FILTER",
-  payload: filter,
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    filterAnecdotes(state, action) {
+      return action.payload.toLowerCase();
+    },
+  },
 });
 
-const reducer = (state = "", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.payload.toLowerCase();
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+export const { filterAnecdotes } = filterSlice.actions;
+export default filterSlice.reducer;
