aed658f6360821955960939e51a9d516f963496e
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2026-01-03 23:34:47 +0100

    feat(ex. 6.20): implement anecdote fetching with React Query

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,18 +1,25 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes} from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  if (result.isError) {
+    return <p>anecdote service not available due to problems in server</p>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
@@ -26,7 +33,7 @@ const App = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+            <button>vote</button>
           </div>
         </div>
       ))}
